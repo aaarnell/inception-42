0ac211a19d8a9c2fa420f70bdd5af79ce39b48f2
aarnell <aarnell@student.21-school.ru>
2022-09-28 19:43:41 +0300

    fix&feat: added wordpress+php

--- srcs/requirements/mariadb/tools/create_db.sql
+++ srcs/requirements/mariadb/tools/create_db.sql
@@ -1,2 +1,8 @@
+CREATE DATABASE wordpress character set utf8 collate utf8_bin;
+
 GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'inception42';
+GRANT ALL ON wordpress.* to 'inception'@'localhost' IDENTIFIED BY 'inception42';
+
 FLUSH PRIVILEGES;
+
+
